b845ba3ca17ed393a39c6850753154f573389c4e
Vatsal Chavda <vatsalchavda2@gmail.com>
2025-12-12 14:35:23 -0500

    fix: clean up

--- backend/auth/auth-config.ts
+++
@@ -1,4 +0,0 @@
-export const authConfig = {
-  loginWith: 'EMAIL',
-  verification: false
-};
--- backend/functions/env-config.ts
+++
@@ -1,4 +0,0 @@
-export const STRIPE_SECRET_KEY: string = process.env.STRIPE_SECRET_KEY || '';
-if (!STRIPE_SECRET_KEY) {
-	throw new Error('Missing STRIPE_SECRET_KEY in environment');
-}
